c19379ac8e20dea054ab50545d4a4c20eb93004e
Ibán Domínguez <domingueznoda@gmail.com>
2022-07-12 21:37:20 +0800

    Updated groupSlice tests

--- src/features/groups/groupsSlice.test.ts
+++ src/features/groups/groupsSlice.test.ts
@@ -21,24 +21,24 @@ describe('groups reducer', () => {
 
   it('should add a Group', () => {
     const group: Group = { id: 1, title: 'MyGroup' }
-    const actual = groupReducer(initialState, addGroup(group))
+    const actual: GroupsState = groupReducer(initialState, addGroup(group))
     expect(actual.list.length).toEqual(1)
   })
 
   it('should update a Group', () => {
     const group: Group = { id: 1, title: 'MyGroup' }
     const updatedGroup: Group = { id: 1, title: 'MyUpdatedGroup' }
-    const actual = groupReducer(initialState, addGroup(group))
+    const actual: GroupsState = groupReducer(initialState, addGroup(group))
     expect(actual.list[0].title).toEqual('MyGroup')
-    const updated = groupReducer(actual, updateGroup(updatedGroup))
+    const updated: GroupsState = groupReducer(actual, updateGroup(updatedGroup))
     expect(updated.list[0].title).toEqual('MyUpdatedGroup')
   })
 
   it('should remove a Group', () => {
     const group: Group = { id: 1, title: 'MyGroup' }
-    const actual = groupReducer(initialState, addGroup(group))
+    const actual: GroupsState = groupReducer(initialState, addGroup(group))
     expect(actual.list.length).toEqual(1)
-    const updated = groupReducer(actual, removeGroup(group))
+    const updated: GroupsState = groupReducer(actual, removeGroup(group))
     expect(updated.list.length).toEqual(0)
   })
 })
